f8982bd4d8f1e7ca86836f5abca9e589ca61f9bf
Kevin Jhang <kevin.jhang@ptlk.com.tw>
2022-10-02 22:16:31 +0800

    Initial commit

---
+++ PTLK.Common.Extensions/CommonExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PTLK.Common.Extensions
+{
+    public static class CommonExtensions
+    {
+        /// <summary>
+        /// Convert Object to Boolean.
+        /// </summary>
+        public static bool ToBoolean(this object? value)
+        {
+            string cleanValue = (value?.ToString() ?? "").Trim();
+            if (string.Equals(cleanValue, "FALSE", StringComparison.OrdinalIgnoreCase) || cleanValue == "0")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
